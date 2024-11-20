63c333663578d0ff1f23319ee28f893063e6aa73
gary.archer <gary.archer@curity.io>
2024-11-20 10:28:27 +0100

    Fixed vulnerabilities

--- spa/webpack/webpack.dev.ts
+++ spa/webpack/webpack.dev.ts
@@ -1,25 +1,34 @@
+// import fs from 'fs';
 import path from 'path';
 import webpack from 'webpack';
 import {merge} from 'webpack-merge';
 import common from './webpack.common.js';
+import webpackDevServer from 'webpack-dev-server';
 
 const dirname = process.cwd();
-const devConfig: webpack.Configuration = {
+const devServer: webpackDevServer.Configuration = {
 
-  mode: 'development',
-  devServer: {
+    /*server: {
+        type: 'https',  
+        options: {
+            key: fs.readFileSync('./certs/example.ssl.key'),
+            cert: fs.readFileSync('./certs/example.ssl.crt'),
+        },
+    },*/
     static: {
         directory: path.join(dirname, './dist'),
     },
     port: 3000,
-    historyApiFallback: {
-        index: 'index.html'
-    },
+    historyApiFallback: true,
     hot: true,
     allowedHosts: [
         'www.product.example', 'login.example.com'
     ],
-  },
+};
+
+const devConfig: webpack.Configuration = {
+    mode: 'development',
+    devServer,
 };
 
 export default merge(common, devConfig);
